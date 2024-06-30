7f7f980018ae626fb580cb0cd67985f64e243bbc
dr666m1 <26474260+kitta65@users.noreply.github.com>
2024-06-30 17:13:48 +0900

    read environment variables

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,32 @@
 import * as vscode from "vscode";
 
+type EnvVal = {
+  name: string;
+  value: string;
+};
+
 export function activate(context: vscode.ExtensionContext) {
+  const envs: EnvVal[] = Object.entries(process.env)
+    .map(([k, v]) => {
+      return { name: k, value: v || "" };
+    })
+    .filter((e) => e.value !== "");
+
   const provider = vscode.languages.registerCompletionItemProvider(
     { pattern: "**" }, // all file types
-    { provideCompletionItems },
+    {
+      provideCompletionItems: () => {
+        const completions = envs.map((e) => {
+          const snippetCompletion = new vscode.CompletionItem(e.name);
+          snippetCompletion.insertText = new vscode.SnippetString(e.value);
+          snippetCompletion.documentation = e.value;
+          return snippetCompletion;
+        });
+        return completions;
+      },
+    },
   );
   context.subscriptions.push(provider);
-}
-
-function provideCompletionItems(
-  _doc: vscode.TextDocument,
-  _pos: vscode.Position,
-  _tkn: vscode.CancellationToken,
-  _ctx: vscode.CompletionContext,
-) {
-  const snippetCompletion = new vscode.CompletionItem("myemail");
-  snippetCompletion.insertText = new vscode.SnippetString(
-    "myemail@example.com${0}",
-  );
-  snippetCompletion.documentation = "insert my email address";
-
-  return [snippetCompletion];
 }
 
 export function deactivate() {}
